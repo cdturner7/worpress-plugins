3fdeb835ad1dfc2c5dc64d70ba2f13882a542ee6
collin <collin.turn@gmail.com>
2023-07-04 23:50:10 -0400

    Update wuth submenus - part 15 of tutorial

--- mod-admin-area/include/API/SettingsAPI.php
+++ mod-admin-area/include/API/SettingsAPI.php
@@ -10,6 +10,7 @@ namespace Include\API;
 class SettingsAPI {
 
     public $admin_pages = [];
+    public $admin_subpages = [];
 
     public function register() {
         
@@ -23,6 +24,34 @@ class SettingsAPI {
         return $this;
     }
 
+    public function addSubpages(array $pages) {
+        $this->admin_subpages = array_merge($this->admin_subpages, $pages);
+        return $this;
+
+    }
+
+    public function withSubpage(string $title = null) {
+        if (empty($this->admin_pages)) {
+            return $this;
+        }
+
+        $admin_page = $this->admin_pages[0];
+        $subpage = [
+            [
+                'parent_slug' => $admin_page['menu_slug'],
+                'page_title'  => $admin_page['page_title'], 
+                'menu_title'  => ($title) ? $title : $admin_page['menu_title'], 
+                'capability'  => $admin_page['capability'], 
+                'menu_slug'   => $admin_page['menu_slug'], 
+                'callback'    => $admin_page['callback']
+            ]
+        ];
+
+        $this->admin_subpages = $subpage;
+
+        return $this;
+    }
+
     public function addAdminMenu() {
         foreach($this->admin_pages as $page) {
             add_menu_page(
@@ -33,6 +62,17 @@ class SettingsAPI {
                 $page['callback'],
                 $page['icon_url'],
                 $page['position']
+            );
+        }
+
+        foreach($this->admin_subpages as $subpage) {
+            add_submenu_page(
+                $subpage['parent_slug'],
+                $subpage['page_title'],
+                $subpage['menu_title'],
+                $subpage['capability'],
+                $subpage['menu_slug'],
+                $subpage['callback']
             );
         }
     }
--- mod-admin-area/include/Pages/Admin.php
+++ mod-admin-area/include/Pages/Admin.php
@@ -15,26 +15,55 @@ class Admin extends BaseController {
     public $settings;
 
     public $pages = [];
+    public $subpages = [];
 
     function __construct() {
         $this->settings = new SettingsAPI();
 
-        $this->pages = [
+        $this->pages =  [
             [
                 'page_title' => 'Modular Administrative Area Settings', 
                 'menu_title' => 'ModAdminArea', 
                 'capability' => 'manage_options', 
-                'menu_slug' => 'mod-admin-area', 
-                'callback' => function() { 
+                'menu_slug'  => 'mod-admin-area', 
+                'callback'   => function() { 
                     echo '<h1>Modular Administrative Area Settings</h1>'; 
                 },
-                'icon_url' => 'dashicons-store',
-                'position' => 110
+                'icon_url'   => 'dashicons-store',
+                'position'   => 110
+            ]
+        ];
+
+        $this->subpages =  [
+            [
+                'parent_slug' => 'mod-admin-area',
+                'page_title'  => 'Modular Admin Area Test1', 
+                'menu_title'  => 'Test1', 
+                'capability'  => 'manage_options', 
+                'menu_slug'   => 'mod-admin-test1', 
+                'callback'    => function() { echo '<h1>CPT Test1</h1>'; }
+            ],
+            [
+                'parent_slug' => 'mod-admin-area',
+                'page_title'  => 'Modular Admin Area Test2', 
+                'menu_title'  => 'Test2', 
+                'capability'  => 'manage_options', 
+                'menu_slug'   => 'mod-admin-test2', 
+                'callback'    => function() { echo '<h1>CPT Test2</h1>'; }
+            ],
+            [
+                'parent_slug' => 'mod-admin-area',
+                'page_title'  => 'Modular Admin Area Test3', 
+                'menu_title'  => 'Test3', 
+                'capability'  => 'manage_options', 
+                'menu_slug'   => 'mod-admin-test3', 
+                'callback'    => function() { echo '<h1>CPT Test3</h1>'; }
             ]
         ];
     }
     
     public function register() {
-        $this->settings->addPages($this->pages)->register();
+        $this->settings->addPages($this->pages)->withSubpage('Dashboard')->
+            addSubpages($this->subpages)->register();
     }
 }
